084860d6fee73cada36427b7ef99ac101e1abfc3
ehitajate-ivanov <137901830+ehitajate-ivanov@users.noreply.github.com>
2025-06-03 16:02:01 +0300

    commit

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,22 @@
 
+const swiper2 = new Swiper(".mySwiper", {
+    loop: true,
+    slidesPerView: 1, // обычно делаем 3, чтобы видеть соседние слайды
+    centeredSlides: true, // активный слайд по центру
+    slideToClickedSlide: true, // клик по слайду двигает его в центр
+    watchSlidesProgress: true, // нужно для "opacity" 
+    freeMode: true,
+    watchSlidesProgress: true,
+})
 
 const swiper = new Swiper(".swiperText", {
     loop: true,
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
-    }
+    },
+    thumbs: {
+        swiper: swiper2, // Связка через thumbs
+    },
 })
+
